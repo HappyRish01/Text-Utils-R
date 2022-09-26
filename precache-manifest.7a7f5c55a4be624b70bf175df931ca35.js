48615c7014ce4114fcb8625356f5680e15c1261c
HappyRish01 <rishk219@gmail.com>
2022-09-26 15:54:15 +0530

    Updates

---
+++ precache-manifest.7a7f5c55a4be624b70bf175df931ca35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd503ef537d44b6af038",
+    "url": "/Text-Utils-R/static/css/main.15ea4ebd.chunk.css"
+  },
+  {
+    "revision": "cd503ef537d44b6af038",
+    "url": "/Text-Utils-R/static/js/main.cd503ef5.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Text-Utils-R/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/Text-Utils-R/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "7ece2f60a485e2f21b81",
+    "url": "/Text-Utils-R/static/js/runtime~main.7ece2f60.js"
+  },
+  {
+    "revision": "5f062b54f79c34829aad8f98854ec68b",
+    "url": "/Text-Utils-R/index.html"
+  }
+];
